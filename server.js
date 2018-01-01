7194be74bea4a391e80e7c906c00746e95a161fe
Carl Lawrence <carl.lawrence@live.com>
2018-01-01 10:33:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var debug = require('debug')('passport-mongo');
 var app = require('./app');
 
 //set server port to 3000
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 //listen for connections on port 3000
 var server = app.listen(app.get('port'), function () {
